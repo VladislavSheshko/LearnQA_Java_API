0f2945d8f878b3205d40b985f2ed583bc73b3519
Vladislav <vladislav_sheshko@mail.ru>
2026-02-28 13:29:06 +0500

    Тесты на метод user

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.BaseTestCase;
 import lib.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testGetUserDataNotAuth() {
@@ -27,8 +30,8 @@ public class UserGetTest extends BaseTestCase {
     @Test
     public void testGetUserDetailsAuthAsSameUser() {
         Map<String,String> authData = new HashMap<>();
-        authData.put("email", "vladsheshko@example.com");
-        authData.put("password","123");
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password","1234");
 
         Response responseGetAuth = RestAssured
                 .given()
@@ -36,17 +39,56 @@ public class UserGetTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
 
-       String header = this.getHeader(responseGetAuth, "x-csrf-token");
-       String cookie = this.getCookie(responseGetAuth, "auth_sid");
+        System.out.println("Полученные данные авторизации:");
+        System.out.println(responseGetAuth.asString());
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
 
         Response responseUserData = RestAssured
                 .given()
                 .header("x-csrf-token", header)
                 .cookie("auth_sid", cookie)
-                .post("https://playground.learnqa.ru/api/user/2")
+                .get("https://playground.learnqa.ru/api/user/2")
                 .andReturn();
 
+        System.out.println("Полученные данные пользователя:");
+        System.out.println(responseUserData.asString());
+
         String[] expectedFields = {"username", "firstName", "lastName", "email"};
-        Assertions.assertJsonHasFields(responseGetAuth, expectedFields);
+        Assertions.assertJsonHasFields(responseUserData, expectedFields);
     }
+
+    @Test
+    public void testGetUserDetailsAuthAsDifferentUser() {
+        Map<String,String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password","1234");
+
+        String userEndpoint = "https://playground.learnqa.ru/api/user/login";
+        Response responseGetAuth = apiCoreRequests.makePostRequest(userEndpoint, authData);
+
+        System.out.println("Полученные данные авторизации:");
+        System.out.println(responseGetAuth.asString());
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+
+        String userEndpointTwo = "https://playground.learnqa.ru/api/user/1";
+        Response responseUserData = apiCoreRequests.makeGetRequest(userEndpointTwo, header, cookie);
+
+        System.out.println("Полученные данные пользователя:");
+        System.out.println(responseUserData.asString());
+
+        // Проверяем, что видим ТОЛЬКО username чужого пользователя
+        String[] visibleFields = {"username"};
+        Assertions.assertJsonHasFields(responseUserData, visibleFields);
+
+        String[] hiddenFields = {"firstName", "lastName", "email"};
+        for (String field : hiddenFields) {
+            Assertions.assertJsonHasNotField(responseUserData, field);
+        }
+    }
+
 }
