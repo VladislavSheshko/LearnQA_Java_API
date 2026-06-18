2a4f9268a20b30c9b2b19d7cbed48a0c8a0dcf40
Vladislav <vladislav_sheshko@mail.ru>
2026-06-18 09:27:54 +0500

    В класс HelloWorldTest добавлено пару новых тестов

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,49 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorldTest(){
-        System.out.println("Hello from Vladislav Shehko");
+        System.out.println("Hello from Vladislav Sheshko");
+    }
+
+
+    @Test
+    public void testGetRequestTest(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Vladislav");
+
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+        // Проверка Content-Type вместо вывода в консоль
+        assertEquals("application/json", response.getHeader("Content-Type"));
+        //System.out.println(response.getHeader("Content-Type"));
+    }
+
+    @Test
+    public void testRestAssured(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Vladislav");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String name = response.get("answer");
+        System.out.println(name);
     }
 }
